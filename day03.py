ad68166c67347c8eca681a7db9f14abc9782f620
codybartfast <john@codybartfast.com>
2026-03-18 20:26:28 +0000

    🖨️

--- day03.py
+++ day03.py
@@ -1,3 +1,22 @@
+#  2017 Day 3
+#  ==========
+#
+#  Part 1: 480
+#  Part 2: 349975
+#
+#  Timings
+#  --------------------------------------
+#      Parse:     0.000001s  (0.958 µs)
+#     Part 1:     0.008401s  (8.401 ms)
+#     Part 2:     0.000065s  (64.83 µs)
+#    Elapsed:     0.008506s  (8.506 ms)
+#  --------------------------------------
+#
+#     Date:  March 2026
+#  Machine:  MacBook M4
+#   Python:  3.14.3
+
+
 from itertools import islice
 
 
@@ -6,39 +25,40 @@ def parse(text):
 
 
 def walk():
-    x, y = (0, 0)
+    x, y = 0, 0
     length = 0
-    yield (x, y)
+    yield x, y
     while True:
         length += 1
         # go right
         for _ in range(length):
             x += 1
-            yield (x, y)
+            yield x, y
         # go up
         for _ in range(length):
             y -= 1
-            yield (x, y)
+            yield x, y
 
         length += 1
         # go left
         for _ in range(length):
             x -= 1
-            yield (x, y)
+            yield x, y
         # go down
         for _ in range(length):
             y += 1
-            yield (x, y)
+            yield x, y
 
 
 def stress_test(target, walk_it):
-    values = {(0, 0): 1}
+    value = 1
+    values = {(0, 0): value}
     next(walk_it)
-    while True:
+    while value <= target:
         x, y = next(walk_it)
         value = sum(
             values.get((x + dx, y + dy), 0)
-            for dx, dy in [
+            for dx, dy in (
                 (-1, -1),
                 (0, -1),
                 (1, -1),
@@ -47,21 +67,19 @@ def stress_test(target, walk_it):
                 (-1, 1),
                 (0, 1),
                 (1, 1),
-            ]
+            )
         )
-        if value > target:
-            return value
-        else:
-            values[(x, y)] = value
+        values[x, y] = value
+    return value
 
 
-def part1(data, args, p1_state):
-    x, y = next(islice(walk(), data - 1, None))
+def part1(steps, args, p1_state):
+    x, y = next(islice(walk(), steps - 1, None))
     return abs(x) + abs(y)
 
 
-def part2(data, args, p1_state):
-    return stress_test(data, iter(walk()))
+def part2(target, args, p1_state):
+    return stress_test(target, iter(walk()))
 
 
 # Runner
