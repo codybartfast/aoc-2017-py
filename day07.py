7ca426c1df21d48eb37d1e26d0ee9c1b92f93ebd
codybartfast <john@codybartfast.com>
2026-03-19 10:59:32 +0000

    day07 part1

---
+++ day07.py
@@ -0,0 +1,44 @@
+def parse(text):
+    def parse_line(line):
+        left, *rest = line.split(" -> ")
+        name, num_str = left.split()
+        weight = int(num_str[1:-1])
+        sub_towers = rest[0].split(", ") if rest else  []
+        return name, weight, sub_towers
+    
+    lines = text.splitlines()
+    return [parse_line(line) for line in lines]
+
+
+def part1(data, args, p1_state):
+    names = set()
+    referenced = set()
+    for name, _, subs in data:
+        names.add(name)
+        referenced.update(subs)
+    return names.difference(referenced).pop()
+
+def part2(data, args, p1_state):
+    return "ans2"
+
+
+# Runner
+################################################################################
+
+
+def jingle(text=None, filepath=None, extra_args=None):
+    if not text and filepath:
+        with open(filepath, "r") as f:
+            text = f.read().strip()
+    sack.present(text, extra_args, parse, part1, part2)
+
+
+if __name__ == "__main__":
+    import sys
+    import sack
+
+    file = sys.argv[1] if len(sys.argv) > 1 else None
+    filepath = sack.get_filepath(file)
+    if filepath:
+        extra_args = sys.argv[2:]
+        jingle(filepath=filepath, extra_args=extra_args)
