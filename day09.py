6dd3022e21dc5ae7a17031d32e95e6481aea0db3
codybartfast <john@codybartfast.com>
2026-03-19 14:13:25 +0000

    day09 part2

--- day09.py
+++ day09.py
@@ -7,6 +7,7 @@ def score_groups(stream):
     in_garbage = False
     ignore = False
     open_groups = 0
+    garbage_count = 0
     for i in range(len(stream)):
         if ignore:
             ignore = False
@@ -15,26 +16,37 @@ def score_groups(stream):
             case "!":
                 ignore = True
             case "<":
+                if in_garbage:
+                    garbage_count += 1
                 in_garbage = True
             case ">":
                 in_garbage = False
             case "{":
-                if not in_garbage:
+                if in_garbage:
+                    garbage_count += 1
+                else:
                     open_groups += 1
             case "}":
-                if not in_garbage:
+                if in_garbage:
+                    garbage_count += 1
+                else:
                     group_score += open_groups
                     open_groups -= 1
-    return group_score
+            case _:
+                if in_garbage:
+                    garbage_count += 1
+
+    return group_score, garbage_count
 
 
 def part1(stream, args, p1_state):
-    print(f"\n{stream}\n")
-    return score_groups(stream)
+    n_groups, n_garbage = score_groups(stream)
+    p1_state.value = n_garbage
+    return n_groups
 
 
-def part2(data, args, p1_state):
-    return "ans2"
+def part2(_, __, p1_state):
+    return p1_state.value
 
 
 # Runner
