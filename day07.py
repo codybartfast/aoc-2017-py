b0aeb77b9c58d722d84df8ab6e58e11d5e65058f
codybartfast <john@codybartfast.com>
2026-03-19 12:11:44 +0000

    day07 part2

--- day07.py
+++ day07.py
@@ -3,23 +3,68 @@ def parse(text):
         left, *rest = line.split(" -> ")
         name, num_str = left.split()
         weight = int(num_str[1:-1])
-        sub_towers = rest[0].split(", ") if rest else  []
-        return name, weight, sub_towers
-    
+        sub_towers = rest[0].split(", ") if rest else []
+        return name, (weight, sub_towers)
+
     lines = text.splitlines()
-    return [parse_line(line) for line in lines]
+    return dict(parse_line(line) for line in lines)
 
 
-def part1(data, args, p1_state):
+def find_base(progs):
     names = set()
     referenced = set()
-    for name, _, subs in data:
+    for name, (_, subs) in progs.items():
         names.add(name)
         referenced.update(subs)
     return names.difference(referenced).pop()
 
+
+def calc_load(name, progs, loads):
+    weight, subs = progs[name]
+    if name in loads:
+        return loads[name]
+
+    weight += sum(calc_load(sub, progs, loads) for sub in subs)
+    loads[name] = weight
+    return weight
+
+
+def find_diff(base, progs, loads):
+    _, subs = progs[base]
+    lds = [loads[sub] for sub in subs]
+    n = lds.pop()
+    if n not in lds:
+        exp, odd = lds.pop(), n
+    else:
+        exp = n
+        odd = [ld for ld in lds if ld != exp][0]
+    return odd - exp
+
+
+def find_odd_weight(name, progs, loads):
+    weight, subs = progs[name]
+    lds = [loads[sub] for sub in subs]
+    if len(set(lds)) == 1:
+        return weight
+    odd_weight = max(lds)
+    odd_name = subs[lds.index(odd_weight)]
+    return find_odd_weight(odd_name, progs, loads)
+    
+
+def part1(data, args, p1_state):
+    base = find_base(data)
+    p1_state.value = base
+    return base
+
+
 def part2(data, args, p1_state):
-    return "ans2"
+    base = p1_state.value
+    loads = {}
+    calc_load(base, data, loads)
+    diff = find_diff(base, data, loads)
+    # No reason can't be negative, but why handle case if Eric's  being kind
+    assert diff > 0 
+    return find_odd_weight(base, data, loads) - diff
 
 
 # Runner
