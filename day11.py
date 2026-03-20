c49ca91ba0856c1f7b85016e4a8413c06590e967
codybartfast <john@codybartfast.com>
2026-03-20 08:52:27 +0000

    day11 part2

--- day11.py
+++ day11.py
@@ -1,38 +1,38 @@
 def parse(text):
     return text.split(",")
 
+
 DELTAS = {
     "nw": (0, 1),
     "n": (1, 1),
     "ne": (1, 0),
     "se": (0, -1),
     "s": (-1, -1),
-    "sw": (-1, 0)
+    "sw": (-1, 0),
 }
 
 
 def walk(pstn, dirs):
     x, y = pstn
+    max_dist = -1
     for dir in dirs:
         dx, dy = DELTAS[dir]
         x += dx
         y += dy
-    return x, y
-
-
-def dist(pstn):
-    # not sure if this is correct in all cases:
-    x, y = pstn
-    return max(abs(x), abs(y)) if x * y > 0 else abs(x) + abs(y)
+        dist = max(abs(x), abs(y)) if x * y > 0 else abs(x) + abs(y)
+        if dist > max_dist:
+            max_dist = dist
+    return dist, max_dist
 
 
 def part1(dirs, args, p1_state):
-    print(f"\n{dirs}\n")
-    return dist(walk((0, 0), dirs))
+    dist, max_dist =walk((0, 0), dirs)
+    p1_state.value = max_dist
+    return dist
 
 
-def part2(data, args, p1_state):
-    return "ans2"
+def part2(_, __, p1_state):
+    return p1_state.value
 
 
 # Runner
