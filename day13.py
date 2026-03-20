feb66bab57cc5706733656a5339af08a3bab5b7b
codybartfast <john@codybartfast.com>
2026-03-20 11:25:43 +0000

    day13 part2

--- day13.py
+++ day13.py
@@ -1,26 +1,52 @@
+#  2017 Day 13
+#  ===========
+#
+#  Part 1: 1624
+#  Part 2: 3923436
+#
+#  Timings
+#  --------------------------------------
+#      Parse:     0.000013s  (13.04 µs)
+#     Part 1:     0.000005s  (5.250 µs)
+#     Part 2:     1.073112s  (1.073 s)
+#    Elapsed:     1.073192s  (1.073 s)
+#  --------------------------------------
+#
+#     Date:  March 2026
+#  Machine:  MacBook M4
+#   Python:  3.14.3
+
 
 def parse(text):
     def parse_line(line):
         parts = line.split(": ")
         return int(parts[0]), int(parts[1])
 
-    return dict(parse_line(line) for line in text.splitlines())
+    return [parse_line(line) for line in text.splitlines()]
 
-def is_caught(wall, depth, time):
-    return depth in wall and time % (2 * (wall[depth] - 1)) == 0 
+def is_caught(depth, range, time):
+    return time % (2 * (range - 1)) == 0 
 
+
+def any_caught(wall, time):
+    return any(is_caught(depth, range, time + depth) for depth, range in wall)
+    
 
 def part1(wall, args, p1_state):
-    size = max(wall.keys()) + 1
     serverity = 0
-    for depth in range(size):
-        if is_caught(wall, depth, depth):
-            serverity += depth * wall[depth]
+    for depth, range in wall:
+        if is_caught(depth, range, depth):
+            serverity += depth * range
     return serverity
 
 
-def part2(data, args, p1_state):
-    return "ans2"
+def part2(wall, args, p1_state):
+    time = -1
+    while True:
+        time += 1
+        if not any_caught(wall, time):
+            return time
+        
 
 
 # Runner
