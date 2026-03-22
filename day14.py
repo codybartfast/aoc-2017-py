5834ca08163661ae5a6c8a3e6dfa23849e199e1c
codybartfast <john@codybartfast.com>
2026-03-22 10:43:29 +0000

    day14 part1

---
+++ day14.py
@@ -0,0 +1,76 @@
+from array import array
+
+def parse(text):
+    return text
+
+
+def knot2(lengths):
+    lengths = list(lengths) + [17, 31, 73, 47, 23]
+    data = list(range(256))
+    pstn = 0
+    skip = 0
+    for _ in range(64):
+        for length in lengths:
+            data = data[pstn:] + data[:pstn]
+            data = list(reversed(data[:length])) + data[length:]
+            data = data[-pstn:] + data[:-pstn]
+            pstn = (pstn + length + skip) % len(data)
+            skip += 1
+    dense = []
+    for start in range(0, 256, 16):
+        n = 0
+        for i in range(start, start + 16):
+            n ^= data[i]
+        dense.append(format(n, "02x"))
+    return "".join(dense)
+    return dense
+
+
+def hex_to_bin(hex):
+    assert not len(hex) % 2
+    bstr = ""
+    for c in hex:
+        bstr += format(int(c, 16), "04b")
+    return bstr
+
+
+def read_disk(key):
+    disk = []
+    for n in range(128):
+        lengths = f"{key}-{n}".encode()
+        disk.append(hex_to_bin(knot2(lengths)))
+    return disk
+    
+
+
+def part1(key, args, p1_state):
+    disk = read_disk(key)
+    p1_state.value = disk
+    return sum(1 for track in disk for b in track if b == "1")
+
+
+
+def part2(data, args, p1_state):
+    return "ans2"
+
+
+# Runner
+################################################################################
+
+
+def jingle(text=None, filepath=None, extra_args=None):
+    if not text and filepath:
+        with open(filepath, "r") as f:
+            text = f.read().strip()
+    sack.present(text, extra_args, parse, part1, part2)
+
+
+if __name__ == "__main__":
+    import sys
+    import sack
+
+    file = sys.argv[1] if len(sys.argv) > 1 else None
+    filepath = sack.get_filepath(file)
+    if filepath:
+        extra_args = sys.argv[2:]
+        jingle(filepath=filepath, extra_args=extra_args)
