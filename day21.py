7a4ead48370dfa0a191224ff69bc0413ff3ce7fe
codybartfast <john@codybartfast.com>
2026-03-27 12:05:16 +0000

    🖨️

--- day21.py
+++ day21.py
@@ -1,144 +1,54 @@
+#  2017 Day 21
+#  ===========
+#
+#  Part 1: 171
+#  Part 2: 2498142
+#
+#  Timings
+#  --------------------------------------
+#      Parse:     0.000027s  (27.42 µs)
+#     Part 1:     0.002073s  (2.073 ms)
+#     Part 2:     0.000055s  (54.58 µs)
+#    Elapsed:     0.002194s  (2.194 ms)
+#  --------------------------------------
+#
+#     Date:  March 2026
+#  Machine:  MacBook M4
+#   Python:  3.14.3
+
+
+# This is a slight mess.  I started with a memoized approach that completed in
+# about 1ms but was incorrect for a reason I never fathomed.  So I wrote the
+# naive solution (which takes around 800ms) and then used that to build the
+# three_to_threes table, where the original bug was likely hiding itself.
+#
+# The memoized approach relies on a 3x3 grid giving us a 9x9 grid after 3
+# iterations which can then be split into 9 3x3 grids.
+#
+# Once we have the three_to_threes table 18 iterations only takes 0.06ms.
+
+
 from itertools import batched
 
 
 def parse(text):
-    def parse_line(line):
-        return line.split(" => ")
 
     lines = text.splitlines()
-    start_len = len(lines[0])
-    two_to_three = []
-    three_to_four = []
+    short = len(lines[0])
+    _2_to_3 = []
+    _3_to_4 = []
 
     for line in lines:
-        if len(line) == start_len:
-            two_to_three.append(parse_line(line))
-        else:
-            three_to_four.append(parse_line(line))
+        (_2_to_3 if len(line) == short else _3_to_4).append(line.split(" => "))
 
-    return dict(two_to_three), dict(three_to_four)
+    return dict(_2_to_3), dict(_3_to_4)
 
 
-def normaliser(dict):
-    def make_key(grid):
-        return "/".join("".join(row) for row in grid)
-
-    normalise = {}
-    for pattern in list(dict.keys()):
-        grid = list(map(list, pattern.split("/")))
-        for _ in range(4):
-            for row in grid:
-                row.reverse()
-            normalise[make_key(grid)] = pattern
-            grid = list(map(list, zip(*grid)))
-            normalise[make_key(grid)] = pattern
-
-    return normalise
+def to_str(grid):
+    return "/".join("".join(row) for row in grid)
 
 
-def split_four(four, normal2):
-    parts = [pair for row in four.split("/") for pair in [row[:2], row[2:]]]
-    return [
-        normal2[f"{parts[0]}/{parts[2]}"],
-        normal2[f"{parts[1]}/{parts[3]}"],
-        normal2[f"{parts[4]}/{parts[6]}"],
-        normal2[f"{parts[5]}/{parts[7]}"],
-    ]
-
-
-def count_pixels(
-    three,
-    iterations,
-    normal2,
-    two_to_three,
-    three_to_four,
-    three_to_threes,
-    known,
-):
-    assert iterations >= 0
-
-    key = (iterations, three)
-    if key in known:
-        return known[key]
-
-    if iterations == 0:
-        value = three.count("#")
-    elif iterations == 1:
-        value = three_to_four[three].count("#")
-    elif iterations == 2:
-        four = three_to_four[three]
-        twos = split_four(four, normal2)
-        threes = [two_to_three[two] for two in twos]
-        value = sum(three.count("#") for three in threes)
-    else:
-        next_threes = three_to_threes[three]
-        value = sum(
-            count_pixels(
-                next_three,
-                iterations - 3,
-                normal2,
-                two_to_three,
-                three_to_four,
-                three_to_threes,
-                known,
-            )
-            for next_three in next_threes
-        )
-    known[key] = value
-    return value
-
-
-def threes_to_twos(threes, normal2):
-    [top_left, top_right, bottom_left, bottom_right] = threes
-    left = top_left.split("/") + bottom_left.split("/")
-    right = top_right.split("/") + bottom_right.split("/")
-    six = [left[i] + right[i] for i in range(6)]
-    return [
-        normal2["/".join([six[y][x : x + 2], six[y + 1][x : x + 2]])]
-        for y in range(0, 6, 2)
-        for x in range(0, 6, 2)
-    ]
-
-
-def split_nine(nine):
-    threes = []
-    for y in range(0, 9, 3):
-        for x in range(0, 9, 3):
-            threes.append(
-                "".join(nine[y][x : x + 3])
-                + "/"
-                + "".join(nine[y + 1][x : x + 3])
-                + "/"
-                + "".join(nine[y + 2][x : x + 3])
-            )
-    return threes
-
-
-def solve(two_to_three, three_to_four, iterations):
-    normal2 = normaliser(two_to_three)
-    normal3 = normaliser(three_to_four)
-
-    three_to_threes = {}
-    for three in three_to_four.keys():
-        nine = solve2(
-            three.split("/"), 3, two_to_three, three_to_four, normal2, normal3
-        )
-        threes = split_nine(nine)
-        three_to_threes[three] = [normal3[three] for three in threes]
-
-    three = ".#./..#/###"
-
-    return count_pixels(
-        normal3[three],
-        iterations,
-        normal2,
-        two_to_three,
-        three_to_four,
-        three_to_threes,
-        {},
-    )
-
-
+# Naive
 ################################################################################
 
 
@@ -146,18 +56,29 @@ def count(grid):
     return sum(1 for line in grid for char in line if char == "#")
 
 
-def extend_row(row, dim, extend, normal):
-    squares = [
+def squares_from_row(row, dim):
+    return [
         [row[y][x : x + dim] for y in range(dim)] for x in range(0, len(row[0]), dim)
     ]
-    str_squares = ["/".join("".join(row) for row in square) for square in squares]
+
+
+def to_squares(grid, dim):
+    rows = batched(grid, dim)
+    return [squares_from_row(row, dim) for row in rows]
+
+
+def row_from_squares(squares, dim):
+    return [[char for square in squares for char in square[y]] for y in range(dim)]
+
+
+def extend_row(row, dim, extend, normal):
+    str_squares = [to_str(square) for square in squares_from_row(row, dim)]
     str_new_squares = [extend[normal[str_square]] for str_square in str_squares]
     new_squares = [str_square.split("/") for str_square in str_new_squares]
-    dim += 1
-    return [[char for square in new_squares for char in square[y]] for y in range(dim)]
+    return row_from_squares(new_squares, dim + 1)
 
 
-def solve2(grid, iterations, two_to_three, three_to_four, normal2, normal3):
+def make_grid(grid, iterations, two_to_three, three_to_four, normal2, normal3):
 
     if not iterations:
         return grid
@@ -177,22 +98,116 @@ def solve2(grid, iterations, two_to_three, three_to_four, normal2, normal3):
         for row in [extend_row(row, dim, extend, normal) for row in rows]
         for line in row
     ]
-    return solve2(grid, iterations - 1, two_to_three, three_to_four, normal2, normal3)
+    return make_grid(
+        grid, iterations - 1, two_to_three, three_to_four, normal2, normal3
+    )
 
 
-def disp(str_sqr):
-    print("\n".join(str_sqr.split("/")))
-    print()
+# Memoized
+################################################################################
+
+
+def count_pixels(
+    three,
+    iterations,
+    known,
+    two_to_three,
+    three_to_four,
+    normal2,
+    normal3,
+    three_to_threes,
+):
+
+    key = (iterations, three)
+    if key in known:
+        return known[key]
+
+    if iterations == 0:
+        value = three.count("#")
+    elif iterations == 1:
+        value = three_to_four[three].count("#")
+    elif iterations == 2:
+        grid = make_grid(
+            three.split("/"), 2, two_to_three, three_to_four, normal2, normal3
+        )
+        value = count(grid)
+    else:
+        value = sum(
+            count_pixels(
+                next_three,
+                iterations - 3,
+                known,
+                two_to_three,
+                three_to_four,
+                normal2,
+                normal3,
+                three_to_threes,
+            )
+            for next_three in three_to_threes[three]
+        )
+    known[key] = value
+    return value
+
+
+def normaliser(dict):
+    normalise = {}
+    for pattern in list(dict.keys()):
+        grid = list(map(list, pattern.split("/")))
+        for _ in range(4):
+            for row in grid:
+                row.reverse()
+            normalise[to_str(grid)] = pattern
+            grid = list(map(list, zip(*grid)))
+            normalise[to_str(grid)] = pattern
+
+    return normalise
+
+
+def make_three_to_threes(two_to_three, three_to_four, normal2, normal3):
+    three_to_threes = {}
+    for three in three_to_four.keys():
+        nine = make_grid(
+            three.split("/"), 3, two_to_three, three_to_four, normal2, normal3
+        )
+        three_to_threes[three] = [
+            normal3[to_str(three)] for row in to_squares(nine, 3) for three in row
+        ]
+    return three_to_threes
 
 
 def part1(design, args, p1_state):
     two_to_three, three_to_four = design
-    return solve(two_to_three, three_to_four, 5)
+    normal2 = normaliser(two_to_three)
+    normal3 = normaliser(three_to_four)
+    three_to_threes = make_three_to_threes(
+        two_to_three, three_to_four, normal2, normal3
+    )
+    p1_state.value = normal2, normal3, three_to_threes
+    return count_pixels(
+        normal3[".#./..#/###"],
+        5,
+        {},
+        two_to_three,
+        three_to_four,
+        normal2,
+        normal3,
+        three_to_threes,
+    )
 
 
 def part2(design, args, p1_state):
     two_to_three, three_to_four = design
-    return solve(two_to_three, three_to_four, 18)
+    normal2, normal3, three_to_threes = p1_state.value
+    return count_pixels(
+        normal3[".#./..#/###"],
+        18,
+        {},
+        two_to_three,
+        three_to_four,
+        normal2,
+        normal3,
+        three_to_threes,
+    )
 
 
 # Runner
