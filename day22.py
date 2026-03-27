8902d7b6cdb5554c2a7786bdc03f4cd169a24ae8
codybartfast <john@codybartfast.com>
2026-03-27 15:50:43 +0000

    🖨️

--- day22.py
+++ day22.py
@@ -6,10 +6,10 @@
 #
 #  Timings
 #  --------------------------------------
-#      Parse:     0.000035s  (34.63 µs)
-#     Part 1:     0.001598s  (1.598 ms)
-#     Part 2:     1.226512s  (1.227 s)
-#    Elapsed:     1.228202s  (1.228 s)
+#      Parse:     0.000037s  (37.12 µs)
+#     Part 1:     0.001144s  (1.144 ms)
+#     Part 2:     0.796101s  (796.1 ms)
+#    Elapsed:     0.797334s  (797.3 ms)
 #  --------------------------------------
 #
 #     Date:  March 2026
@@ -17,64 +17,75 @@
 #   Python:  3.14.3
 
 
+#  Using an array for Part 2 reduced overall time from around 1,200ms to 800ms
+
+
+from array import array
+
+
 DIRS = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+MAX = 225
 
 
 def parse(text):
     lines = text.splitlines()
     dim = len(lines)
     mid = dim // 2
+    offset = MAX - mid
     nodes = {}
     for y in range(dim):
         for x in range(dim):
             if lines[y][x] == "#":
-                nodes[x, y] = 2
+                nodes[offset + x, offset + y] = 2
 
-    return (mid, mid), nodes
+    return (MAX, MAX), nodes
 
 
-def spread(pstn, nodes, bursts):
+def spread(curr, nodes, bursts):
     dir = 0
     infections = 0
 
     for _ in range(bursts):
-        infected = nodes.get(pstn, False)
+        infected = nodes.get(curr, False)
         dir = (dir + (1 if infected else -1)) % 4
         delta = DIRS[dir]
         if infected:
-            nodes[pstn] = False
+            nodes[curr] = False
         else:
-            nodes[pstn] = True
+            nodes[curr] = True
             infections += 1
-        pstn = pstn[0] + delta[0], pstn[1] + delta[1]
+        curr = curr[0] + delta[0], curr[1] + delta[1]
 
     return infections
 
 
-def evolved(pstn, nodes, bursts):
+def evolved(curr, nodes, bursts):
+    width = 2 * MAX + 1
+    cluster = array("B", [0] * width * width)
+    for x, y in nodes.keys():
+        cluster[y * width + x] = 2
+    idx = curr[1] * width + curr[0]
+    dirs = [-width, 1, width, -1]
+
     dir = 0
     infections = 0
-
     for _ in range(bursts):
-        state = nodes.get(pstn, 0)
-        nodes[pstn] = (state + 1) % 4
-        dir = (dir + 3 + state) % 4
-        delta = DIRS[dir]
+        cluster[idx] = ((state := cluster[idx]) + 1) % 4
         if state == 1:
             infections += 1
-        pstn = pstn[0] + delta[0], pstn[1] + delta[1]
+        idx += dirs[dir := (dir + 3 + state) % 4]
 
     return infections
 
 
-def part1(data, args, p1_state):
-    pstn, nodes = data
-    return spread(pstn, dict(nodes), 10_000)
+def part1(status, args, p1_state):
+    curr, nodes = status
+    return spread(curr, dict(nodes), 10_000)
 
 
-def part2(data, args, p1_state):
-    pstn, nodes = data
-    return evolved(pstn, nodes, 10_000_000)
+def part2(status, args, p1_state):
+    curr, nodes = status
+    return evolved(curr, nodes, 10_000_000)
 
 
 # Runner
