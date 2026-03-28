15e767508706a0e01a95c8c778d961fce3c24334
codybartfast <john@codybartfast.com>
2026-03-28 12:18:27 +0000

    🖨️

--- day23.py
+++ day23.py
@@ -1,11 +1,43 @@
+#  2017 Day 23
+#  ===========
+#
+#  Part 1: 5929
+#  Part 2: 907
+#
+#  Timings
+#  --------------------------------------
+#      Parse:     0.000029s  (29.25 µs)
+#     Part 1:     0.003702s  (3.702 ms)
+#     Part 2:     3.970636s  (3.971 s)
+#    Elapsed:     3.974460s  (3.974 s)
+#  --------------------------------------
+#
+#     Date:  March 2026
+#  Machine:  MacBook M4
+#   Python:  3.14.3
+
+
 REG_NAMES = ["a", "b", "c", "d", "e", "f", "g", "h"]
+
+REG_A = REG_NAMES.index("a")
 REG_B = REG_NAMES.index("b")
+REG_C = REG_NAMES.index("c")
 REG_D = REG_NAMES.index("d")
+REG_E = REG_NAMES.index("e")
 REG_F = REG_NAMES.index("f")
+REG_G = REG_NAMES.index("g")
 REG_H = REG_NAMES.index("h")
 
-OPS = ["jnz", "mul", "set", "sub", "patch"]
-#       0      1      2      3      4        5      6
+
+OPS = ["jnz", "mul", "set", "sub", "mod"]
+#       0      1      2      3      4
+# match case values are hard coded
+
+JNZ = OPS.index("jnz")
+MUL = OPS.index("mul")
+SET = OPS.index("set")
+SUB = OPS.index("sub")
+MOD = OPS.index("mod")
 
 
 def parse(text):
@@ -33,16 +65,7 @@ def run(prog, regs):
         match op:
             # jnz
             case 0:
-                # pc += (regs[a2] if t2 else a2) if (regs[a1] if t1 else a1) else 1
-                if regs[a1] if t1 else a1:
-                    pc += regs[a2] if t2 else a2
-                else:
-                    pc += 1
-            # mul
-            case 1:
-                regs[a1] = regs[a1] * (regs[a2] if t2 else a2)
-                pc += 1
-                n_mul += 1
+                pc += (regs[a2] if t2 else a2) if (regs[a1] if t1 else a1) else 1
             # set
             case 2:
                 regs[a1] = regs[a2] if t2 else a2
@@ -51,17 +74,18 @@ def run(prog, regs):
             case 3:
                 regs[a1] = regs[a1] - (regs[a2] if t2 else a2)
                 pc += 1
-            # patch
+            # mod
             case 4:
-                b = regs[REG_B]
-                d = regs[REG_D]
-                if b % d == 0:
-                    regs[REG_F] = 0
-                    pc += 15
-                else:
-                    pc += 10
+                regs[a1] = regs[a1] % (regs[a2] if t2 else a2)
+                pc += 1
+            # mul
+            case 1:
+                regs[a1] = regs[a1] * (regs[a2] if t2 else a2)
+                pc += 1
+                n_mul += 1
             case _:
                 assert False, prog[pc]
+
     return n_mul, regs[REG_H]
 
 
@@ -70,9 +94,15 @@ def part1(prog, args, p1_state):
 
 
 def part2(prog, args, p1_state):
-    prog = list(prog)
-    prog[10] = (OPS.index("patch"), (-1, -1), (-1, -1))
-    prog = tuple(prog)
+    prog[10] = SET, (1, REG_G), (1, REG_B)
+    prog[11] = MOD, (1, REG_G), (1, REG_D)
+    prog[12] = JNZ, (1, REG_G), (0, 8)
+    prog[13] = SET, (1, REG_F), (0, 0)
+    prog[14] = JNZ, (0, 1), (0, 11)
+
+    for i in range(15, 20):
+        prog[i] = None
+
     return run(prog, boot(1))[1]
 
 
