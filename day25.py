9d16a25db3398d085eb503a340d693c456689cd7
codybartfast <john@codybartfast.com>
2026-03-29 09:30:12 +0100

    🖨️

--- day25.py
+++ day25.py
@@ -1,26 +1,45 @@
+#  2017 Day 25
+#  ===========
+#
+#  Part 1: 4385
+#  Part 2: Merry Christmas
+#
+#  Timings
+#  --------------------------------------
+#      Parse:     0.000024s  (24.08 µs)
+#     Part 1:     0.479060s  (479.1 ms)
+#     Part 2:     0.000002s  (1.750 µs)
+#    Elapsed:     0.479141s  (479.1 ms)
+#  --------------------------------------
+#
+#     Date:  March 2026
+#  Machine:  MacBook M4
+#   Python:  3.14.3
+
+
 from array import array
 from itertools import batched
 
 
 def parse(text):
     lines = text.splitlines()
-
     begin = ord(lines[0][:-1].split()[-1]) - ord("A")
     steps = int(lines[1].split()[-2])
     blocks = list(batched(lines[2:], 10))
     sentences = [[line[:-1].split()[-1] for line in block[1:]] for block in blocks]
-
-    return begin, steps, tuple(
+    prog = tuple(
         (
             (int(ws[2]), (-1 if ws[3] == "left" else 1), ord(ws[4]) - ord("A")),
             (int(ws[6]), (-1 if ws[7] == "left" else 1), ord(ws[8]) - ord("A")),
         )
         for ws in sentences
     )
+    return begin, steps, prog
+
 
 def part1(blueprint, args, p1_state):
     state, steps, prog = blueprint
-    tape = array('B', [0] * (2 ** 14))
+    tape = array("B", [0] * (2**14))
     pstn = len(tape) // 2
 
     for _ in range(steps):
